b08b6889e9a63d636a06dd3055dcae7181dc6277
EC2 Default User <ec2-user@ip-172-31-2-30.ec2.internal>
2021-08-24 07:01:47 +0000

    [finish]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,23 +8,32 @@ class TodolistsController < ApplicationController
     list.save # ２. データをデータベースに保存するためのsaveメソッド実行
     redirect_to todolist_path(list.id) # 詳細画面へリダイレクト
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
   end
 
   private # ストロングパラメータ
